c06832471b1faa23a7fdd14599770bfc123837fd
Salaka Navinda <mmsnavinda@gmail.com>
2021-05-27 12:03:23 +0530

    added

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+import { StockComponent } from './components/stock/stock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
